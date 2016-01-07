e3d776558855cd02038bb12aa78f0120cdb69753
agilewalker <wuhuiyuan@gmail.com>
2016-01-07 16:27:08 +0800

    Initial Commit

---
+++ SwiftyChardet/SwiftyChardet.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyChardet.h
+//  SwiftyChardet
+//
+//  Created by wu on 1/7/16.
+//  Copyright © 2016 Wu Hui Yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyChardet.
+FOUNDATION_EXPORT double SwiftyChardetVersionNumber;
+
+//! Project version string for SwiftyChardet.
+FOUNDATION_EXPORT const unsigned char SwiftyChardetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyChardet/PublicHeader.h>
+
+
